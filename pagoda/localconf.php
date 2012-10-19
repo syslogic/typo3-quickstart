e53afb558668b530eb4d78400e27cb5fb8a1b6b6
syslogic <martin@codefx.biz>
2012-10-19 03:50:14 +0200

    max post size fixed; install password: pagodabox default site name changed.

--- pagoda/localconf.php
+++ pagoda/localconf.php
@@ -3,10 +3,10 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
+$TYPO3_CONF_VARS['SYS']['sitename'] = 'TYPO3 Quicklaunch';
 
-	// Default password is "joh316" :
-$TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
+// Default password is "pagodabox" :
+$TYPO3_CONF_VARS['BE']['installToolPassword'] = '1d61362a25730ba56966b15b49e10345';
 
 $TYPO3_CONF_VARS['EXT']['extList'] = 'info,perm,func,filelist,extbase,fluid,about,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports,felogin,form,introduction';
 
@@ -15,8 +15,8 @@ $typo_db_extTableDef_script = 'extTables.php';
 /* Database Connection for Pagoda Box */
 $typo_db_host = $_SERVER['DB1_HOST'];
 $typo_db = $_SERVER['DB1_NAME'];
-//$typo_db_username = $_SERVER['DB1_USER'];
-//$typo_db_password = $_SERVER['DB1_PASS'];
+$typo_db_username = $_SERVER['DB1_USER'];
+$typo_db_password = $_SERVER['DB1_PASS'];
 
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
 ?>
