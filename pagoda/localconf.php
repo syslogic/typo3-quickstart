8d4aba108aaa425dd377d386bc9ca3d1302d489b
syslogic <martin@codefx.biz>
2012-10-19 07:34:37 +0200

    added another reverse proxy IP

--- pagoda/localconf.php
+++ pagoda/localconf.php
@@ -11,23 +11,19 @@ if(!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-/* default site title */
+/* the site's title */
 $TYPO3_CONF_VARS['SYS']['sitename'] = 'Pagoda TYPO3 Quicklaunch';
-
-/* configuring the load-balancer as reverse proxy */
-$TYPO3_CONF_VARS['SYS']['reverseProxyIP'] = '10.60.5.14';
-$TYPO3_CONF_VARS['SYS']['reverseProxySSL'] = '10.60.5.14';
-$TYPO3_CONF_VARS['SYS']['reverseProxyHeaderMultiValue'] = 'last';
 
 /* the default install password is "joh316" */
 $TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
 
-/* baseurl & cookie domain */
-$TYPO3_CONF_VARS['FE']['defaultTypoScript_constants'] = '[GLOBAL]'.chr(10).'myBaseUrl = http://'.$_SERVER['HTTP_HOST'];
-
-
 /* some typo3 extensions */
 $TYPO3_CONF_VARS['EXT']['extList'] = 'info,perm,func,filelist,extbase,fluid,about,tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports,felogin,form,introduction';
+
+/* configuring the load-balancer as reverse proxy - probably incomplete */
+$TYPO3_CONF_VARS['SYS']['reverseProxyIP'] = '10.60.5.4,10.60.5.14';
+$TYPO3_CONF_VARS['SYS']['reverseProxySSL'] = '10.60.5.4,10.60.5.14';
+$TYPO3_CONF_VARS['SYS']['reverseProxyHeaderMultiValue'] = 'last';
 
 /* the data connection */
 $typo_db_host = $_SERVER['DB1_HOST'];
