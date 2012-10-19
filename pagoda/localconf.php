076ed34a9346fbf527c536f0d0a9ac0e0361887e
syslogic <martin@codefx.biz>
2012-10-19 02:19:33 +0200

    and commented out again.

--- pagoda/localconf.php
+++ pagoda/localconf.php
@@ -13,13 +13,14 @@ $TYPO3_CONF_VARS['EXT']['extList'] = 'info,perm,func,filelist,extbase,fluid,abou
 $typo_db_extTableDef_script = 'extTables.php';
 
 /* fix for mySQL v5.5 */
-$TYPO3_CONF_VARS['SYS']['setDBinit'] = "SET SESSION sql_mode='MYSQL40';";
+//$TYPO3_CONF_VARS['SYS']['setDBinit'] = "SET SESSION sql_mode='MYSQL40';\nSET NAMES utf8;";
+$TYPO3_CONF_VARS['SYS']['setDBinit'] = "SET NAMES utf8;";
 
 /* Database Connection for Pagoda Box */
 $typo_db = $_SERVER['DB1_NAME'];
-$typo_db_username = $_SERVER['DB1_USER'];
-$typo_db_password = $_SERVER['DB1_PASS'];
-$typo_db_host = $_SERVER['DB1_HOST'];
+// $typo_db_username = $_SERVER['DB1_USER'];
+// $typo_db_password = $_SERVER['DB1_PASS'];
+// $typo_db_host = $_SERVER['DB1_HOST'];
 
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
 ?>
